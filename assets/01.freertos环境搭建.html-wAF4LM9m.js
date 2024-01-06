6cbabfd0e112ee503d2a18b61c3c86c69b16f3c0
1663784811 <1663784811@qq.com>
2024-01-06 21:09:51 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@c3888738e43b0fa9c6fd5b79142d1579803f7edf 🚀

---
+++ assets/01.freertos环境搭建.html-wAF4LM9m.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as i,b as a}from"./app-njWDhDvz.js";const n="/assets/01img-udj3l8EI.png",r="/assets/02img-ty62HeAb.png",t="/assets/03img-XDMM3UAC.png",d={},c=a(`<h3 id="freertos-下载地址" tabindex="-1"><a class="header-anchor" href="#freertos-下载地址" aria-hidden="true">#</a> freertos 下载地址</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>https://github.com/FreeRTOS/FreeRTOS
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="目录结构" tabindex="-1"><a class="header-anchor" href="#目录结构" aria-hidden="true">#</a> 目录结构</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+FreeRTOS
+
+FreeRTOS-Plus
+
+tools
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="新建工程" tabindex="-1"><a class="header-anchor" href="#新建工程" aria-hidden="true">#</a> 新建工程</h3><p><img src="`+n+'" alt="01img.png"></p><p><img src="'+r+'" alt="02img.png"></p><p><img src="'+t+'" alt="03img.png"></p>',8),l=[c];function o(m,u){return s(),i("div",null,l)}const v=e(d,[["render",o],["__file","01.freertos环境搭建.html.vue"]]);export{v as default};
---
+++ assets/03.代码.html-M1OTCsFJ.js
@@ -0,0 +1,13 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as n,b as s}from"./app-njWDhDvz.js";const d={},l=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+
+
+
+Button button = findViewById(R.id.button);
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Log.i(&quot;点击&quot;, &quot;onClick: &quot;);
+    }
+});
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),t=[l];function c(r,a){return i(),n("div",null,t)}const u=e(d,[["render",c],["__file","03.代码.html.vue"]]);export{u as default};
