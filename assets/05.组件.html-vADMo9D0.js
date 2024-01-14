1d8a8709e6bcbbd462f8a28bb7d1817b44ee62a6
1663784811 <1663784811@qq.com>
2024-01-14 16:53:25 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@f8a48b4c8614f3a5ebef99eea2da5aac304d51e0 🚀

---
+++ assets/05.组件.html-vADMo9D0.js
@@ -0,0 +1,27 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as e,a as s}from"./app-aUcxWysh.js";const l={},d=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+滚动视图
+    
+    ScrollView
+    
+    
+    HorizontalScrollView
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),v=[d];function a(c,r){return i(),e("div",null,v)}const t=n(l,[["render",a],["__file","05.组件.html.vue"]]);export{t as default};
