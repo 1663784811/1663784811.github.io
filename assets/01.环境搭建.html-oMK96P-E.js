33c46144b8e32916d9a37680c4bfe3a8ddfc01e3
1663784811 <1663784811@qq.com>
2024-01-15 01:17:29 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@524ef92f6ece25d559cefe680733b04450de8b82 🚀

---
+++ assets/01.环境搭建.html-oMK96P-E.js
@@ -0,0 +1,19 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a as d}from"./app-ynW8OMhH.js";const s={},l=d(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>安装jdk环境
+
+
+下载android studio
+https://developer.android.google.cn/studio
+
+
+创建工程：
+
+
+
+安装模拟器：
+
+
+日志打印：
+Log.i(&quot;=======================================:::&quot;,&quot;onCreate: 执行onCreate方法&quot;)
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),a=[l];function r(v,c){return n(),i("div",null,a)}const u=e(s,[["render",r],["__file","01.环境搭建.html.vue"]]);export{u as default};
--- assets/03.CMake命令.html-PSkQR9BD.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a as s}from"./app-53hlVwRj.js";const a={},c=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>## 编译
-cmake .
-cmake ..
-
-
-
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),t=[c];function d(l,r){return n(),i("div",null,t)}const m=e(a,[["render",d],["__file","03.CMake命令.html.vue"]]);export{m as default};
