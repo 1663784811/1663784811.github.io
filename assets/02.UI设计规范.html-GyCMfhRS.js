c8e6adc393e8ee29a89160c771b2beed6e81a79b
1663784811 <1663784811@qq.com>
2024-01-29 10:47:09 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@ed0f1836c737febb33620b4c394fc0701b2b5c95 🚀

--- assets/02.UI设计规范.html-GyCMfhRS.js
+++
@@ -1,9 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a}from"./app-j0OAJ5Q6.js";const s="/assets/01img-hpPFM6e6.png",t={},r=a('<h2 id="tabbar-示例" tabindex="-1"><a class="header-anchor" href="#tabbar-示例" aria-hidden="true">#</a> tabBar 示例</h2><p><img src="'+s+`" alt="img.png"></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>一倍图390 组件标准
-    tabBar
-        高: 49px
-        有icon
-            icon: 盒子20x20  icon高:16px 宽:自适应
-            字: 14px 不多于4个字 margin-top:4px
-        无icon
-            字: 16px
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),c=[r];function d(l,o){return n(),i("div",null,c)}const v=e(t,[["render",d],["__file","02.UI设计规范.html.vue"]]);export{v as default};
