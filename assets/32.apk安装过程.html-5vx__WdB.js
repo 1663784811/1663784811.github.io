d21cf73e533fbad481106191c2b26f6a6bf04817
1663784811 <1663784811@qq.com>
2024-01-30 14:52:05 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@193a31c2abd35f9e3159bcca2f13d66a35f4b3d7 🚀

---
+++ assets/32.apk安装过程.html-5vx__WdB.js
@@ -0,0 +1,12 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as n,a as i}from"./app-cfB73QSR.js";const s={},d=i(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+/system/app    系统自带的应用程序
+
+1.复制apk       到     /data/app/包名称/xxx.apk
+2.复制dex       到     /data/dalvik-cache/x86/
+3.apk解压资源    到     /data/data/包名
+4.记录包名图标等  到     /data/system/packages.xml
+
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),l=[d];function t(c,r){return a(),n("div",null,l)}const _=e(s,[["render",t],["__file","32.apk安装过程.html.vue"]]);export{_ as default};
---
+++ assets/SpringBoot.html-d1mMb36K.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as t}from"./app-cfB73QSR.js";const r={};function c(n,_){return e(),t("div")}const f=o(r,[["render",c],["__file","SpringBoot.html.vue"]]);export{f as default};
