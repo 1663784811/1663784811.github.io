9004c850fd668ca0851d5b02525c9af8153b03fc
1663784811 <1663784811@qq.com>
2024-02-12 10:11:27 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@980bcc5a097e2a2c0e4ccbcdc47ec90107875b8e 🚀

---
+++ assets/04.SPI通信.html-_vz8Sr9o.js
@@ -0,0 +1,11 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as a,a as n}from"./app-64Wgvq_G.js";const s="/assets/spi001-K6AuwA0I.png",d={},r=n(`<p>SPI，是一种高速的，全双工，同步的通信总线，并且在芯片的管脚上只占用四根线</p><h3 id="spi通信具有以下几个主要特点" tabindex="-1"><a class="header-anchor" href="#spi通信具有以下几个主要特点" aria-hidden="true">#</a> SPI通信具有以下几个主要特点：</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. 全双工（Full-Duplex）通信：主设备和从设备可以同时发送和接收数据，实现了同时的双向数据传输。
+2. 同步传输：通信的主设备和从设备需要共享时钟信号，以确保数据的同步传输。
+3. 串行通信：数据以位的形式通过单个引脚进行传输，逐位进行
+4. 硬件控制：SPI通信不需要复杂的协议来控制通信，而是通过硬件信号进行控制。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="引脚定义" tabindex="-1"><a class="header-anchor" href="#引脚定义" aria-hidden="true">#</a> 引脚定义</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+SDI（数据输入）
+SDO（数据输出）
+SCK（时钟）
+CS（片选）
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="接线原理图" tabindex="-1"><a class="header-anchor" href="#接线原理图" aria-hidden="true">#</a> 接线原理图</h3><p><img src="`+s+'" alt="spi001.png"></p>',7),l=[r];function t(c,u){return i(),a("div",null,l)}const m=e(d,[["render",t],["__file","04.SPI通信.html.vue"]]);export{m as default};
