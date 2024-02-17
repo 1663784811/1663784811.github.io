6695ee9990908c5736ac900d7e11139c7d3cf0a7
1663784811 <1663784811@qq.com>
2024-02-17 17:32:00 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@d95da539242565c5e5731d35a45c8b1f199a270d 🚀

---
+++ assets/jvm05-对象.html-5dq4haU2.js
@@ -0,0 +1,12 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as t,c as o,b as e,d as c}from"./app-yslt8qjs.js";const r={},_=e("h3",{id:"对象布局",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#对象布局","aria-hidden":"true"},"#"),c(" 对象布局")],-1),a=e("pre",null,[e("code",null,`对象头：
+        对象标记
+            哈希码
+            GC标记
+            GC次数
+            同步锁标记
+            偏向锁持有者
+        类元信息（类型指针）
+实例数据：
+    
+对齐填充：
+`)],-1),s=[_,a];function d(l,i){return t(),o("div",null,s)}const f=n(r,[["render",d],["__file","jvm05-对象.html.vue"]]);export{f as default};
