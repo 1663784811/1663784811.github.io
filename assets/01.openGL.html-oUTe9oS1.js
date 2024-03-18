fc6894ea24cc2556e92856b461a8aeb7705a2a4e
1663784811 <1663784811@qq.com>
2024-03-18 17:25:03 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@c5b271acd2f52911c642177636eafc51a3d7b12b 🚀

---
+++ assets/01.openGL.html-oUTe9oS1.js
@@ -0,0 +1,11 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a}from"./app-_GkptCgM.js";const s={},d=a(`<h2 id="官网" tabindex="-1"><a class="header-anchor" href="#官网" aria-hidden="true">#</a> 官网</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>https://www.opengl.org/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>OpenGL 开放图形库  是用于渲染2D、3D矢量图形的跨语言、跨平台的应用程序编程接口（API）
+OpenGL常用于CAD、虚拟现实、科学可视化程序和电子游戏开发。
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>GLEW</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>https://glew.sourceforge.net/
+OpenGL Extension Wrangler Library (GLEW) 是一个跨平台开源 C/C++ 扩展加载库。
+GLEW 提供了高效的运行时机制来确定目标平台支持哪些 OpenGL 扩展。
+OpenGL 核心和扩展功能在单个头文件中公开。
+GLEW 已在多种操作系统上进行了测试，
+包括 Windows、Linux、Mac OS X、FreeBSD、Irix 和 Solaris。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),r=[d];function t(l,c){return n(),i("div",null,r)}const u=e(s,[["render",t],["__file","01.openGL.html.vue"]]);export{u as default};
