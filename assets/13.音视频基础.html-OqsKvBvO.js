10f37ce33649b946ea0934b40fc93580efa24cc1
1663784811 <1663784811@qq.com>
2024-03-27 13:05:00 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@a7c7981ff49586c492c2bc050939f4bdfa93cff8 🚀

---
+++ assets/13.音视频基础.html-OqsKvBvO.js
@@ -0,0 +1,21 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as n,a as d}from"./app-16z2Sw2W.js";const s={},a=d(`<h2 id="常见视频格式" tabindex="-1"><a class="header-anchor" href="#常见视频格式" aria-hidden="true">#</a> 常见视频格式</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>H264 H265
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="常见音频格式" tabindex="-1"><a class="header-anchor" href="#常见音频格式" aria-hidden="true">#</a> 常见音频格式</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>ACC
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>视频通话过程</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>=====  采集
+    1.采集视频、音频
+    2.压缩视频、音频 ( 编码 )
+    3.封装成包
+    
+    
+=====  传输
+    底层协议: udp tcp
+    应用层协议: rtmp rtsp webrtc 
+
+
+=====  播放
+    1.拆包
+    2.解压（解码）
+    3.
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),l=[a];function r(c,t){return i(),n("div",null,l)}const m=e(s,[["render",r],["__file","13.音视频基础.html.vue"]]);export{m as default};
