4f807e9dd8c241a574dc1f976659f39a65539aa4
1663784811 <1663784811@qq.com>
2024-04-03 08:36:24 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@41132fd96a24beb214f0bc2fdc715a9e408ef808 🚀

---
+++ assets/4.包.html-l0XBm_v1.js
@@ -0,0 +1,14 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as e,a}from"./app-CeeaJpDY.js";const i={},l=a(`<p>关键字：</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code>使用方法
+<span class="token number">1</span> 
+    <span class="token keyword">import</span> 模块名
+    模块名<span class="token punctuation">.</span>方法<span class="token punctuation">(</span><span class="token punctuation">)</span>
+    
+    <span class="token keyword">import</span> 模块名 <span class="token keyword">as</span> 别名
+
+<span class="token number">2</span>
+    <span class="token keyword">from</span> 模块名 <span class="token keyword">import</span> 方法名
+    方法名<span class="token punctuation">(</span><span class="token punctuation">)</span>
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[l];function t(o,d){return s(),e("div",null,c)}const u=n(i,[["render",t],["__file","4.包.html.vue"]]);export{u as default};
