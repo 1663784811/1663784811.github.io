019fc188b10de69f7444596d91fdcff61c411de6
1663784811 <1663784811@qq.com>
2024-04-25 07:34:48 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@582636b5b44516afd20249a91796ae70f12b47af 🚀

---
+++ assets/08.Service.html-k66TnB0_.js
@@ -0,0 +1,11 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as n,a as s}from"./app-yAbbl3Ah.js";const d={},r=s(`<h3 id="service" tabindex="-1"><a class="header-anchor" href="#service" aria-hidden="true">#</a> Service</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>kubectl get svc
+
+
+1. ClusterIP
+
+
+2.NodePort
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),a=[r];function c(l,t){return i(),n("div",null,a)}const u=e(d,[["render",c],["__file","08.Service.html.vue"]]);export{u as default};
