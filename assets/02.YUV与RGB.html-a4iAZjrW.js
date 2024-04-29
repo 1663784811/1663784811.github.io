dc9994c27169cc44c7c078a0ff78f99724a8b892
1663784811 <1663784811@qq.com>
2024-04-29 20:03:30 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@35ec45764efa1adc92be269a79cf48fa97ffc324 🚀

---
+++ assets/02.YUV与RGB.html-a4iAZjrW.js
@@ -0,0 +1,6 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as a,a as i}from"./app-X9DILhSX.js";const s={},d=i(`<h3 id="色彩空间" tabindex="-1"><a class="header-anchor" href="#色彩空间" aria-hidden="true">#</a> 色彩空间</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>YUV格式    (亮度:Y   U、V 表示色度 )   yuv444 yuv422 yuv420  最早用于电视领域
+RGB格式    (红：R    绿：G    蓝：B   )
+
+HSV/HSL   (色调：H   饱和度：S  亮度: V )
+CMYK
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>YUV格式与RGB格式</p><p>YUV ，是一种颜色编码方法</p><p>yuv420协议图</p>`,5),t=[d];function r(c,l){return n(),a("div",null,t)}const v=e(s,[["render",r],["__file","02.YUV与RGB.html.vue"]]);export{v as default};
