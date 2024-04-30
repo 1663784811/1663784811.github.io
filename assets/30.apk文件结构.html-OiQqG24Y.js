a0ca38a829e075464e6952ecdc2882db67b2f10f
1663784811 <1663784811@qq.com>
2024-04-30 22:10:20 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@d44bf9f99ff1ee2c471fda5521f20753d855dcb7 🚀

---
+++ assets/30.apk文件结构.html-OiQqG24Y.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as n,a as i}from"./app-mXAz_BMf.js";const a={},d=i(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+1.assets  不需要通过编译的资源
+2.res     需要通过aapt编译的资源索引表当中会生成索引
+3.lib     存放so文件
+4.resources.arsc    资源索引表
+5.META-INF  签名文件
+6.*.dex     存放java代码可执行文件
+7.AndroidManifest.xml   配置清单文件
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),l=[d];function r(t,c){return s(),n("div",null,l)}const _=e(a,[["render",r],["__file","30.apk文件结构.html.vue"]]);export{_ as default};
