0dcd15326aa7b99ce13c46942d458f191907a53b
1663784811 <1663784811@qq.com>
2024-06-12 23:07:35 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@2e576df4b9e34ca5860fc30f70ee4329f0c8eea1 🚀

--- assets/01.redis安装.html-ZSJuZ3c1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as _,c as r}from"./app-6_i_Rtq6.js";const t={};function c(o,s){return _(),r("div")}const f=e(t,[["render",c],["__file","01.redis安装.html.vue"]]);export{f as default};
---
+++ assets/06.电池组.html-D6fGinzm.js
@@ -0,0 +1,5 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as l,a as n}from"./app-dthwZN-f.js";const s={},t=n(`<ol><li><p>同型号的电池可以正极与正极、负极与负极连接组成一个大容量电池</p></li><li><p>电源保护板:</p></li><li><p>放电电流</p></li><li><p>充电电流</p></li><li><p>电池内阻</p></li><li><p>放电倍率: 表示放电快慢的一种量度</p></li></ol><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>一般充放电电流的大小常用充放电倍率C来表示，
+    即：充放电倍率C=充放电电流/额定容量；
+    例如：额定容量为100Ah的电池用20A放电时，其放电倍率为0.2C。
+电池放电C率，1C,2C,0.2C是电池放电速率
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),a=[t];function c(d,r){return i(),l("div",null,a)}const p=e(s,[["render",c],["__file","06.电池组.html.vue"]]);export{p as default};
