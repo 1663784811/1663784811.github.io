5030a6f5155d5aa0a8ed9a4e287f443a6dfb3d74
1663784811 <1663784811@qq.com>
2024-07-19 07:49:36 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@7d50f6f9dcbb3371a258b6db39079d4cad951a44 🚀

---
+++ assets/01.环境搭建.html-nwU5P-ke.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as n,a as s}from"./app-eKbJYqPF.js";const l={},d=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>下载： 
+    https://github.com/niXman/mingw-builds-binaries/releases
+    版本: x86_64-12.2.0-release-posix-seh-rt_v10-rev1.7z
+
+
+解压：D:\\c\\mingw64
+
+配置到path
+
+配置 clion
+    file-&gt;setting-&gt;build-&gt;toolchains-&gt; +
+    添加
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),a=[d];function t(c,r){return i(),n("div",null,a)}const u=e(l,[["render",t],["__file","01.环境搭建.html.vue"]]);export{u as default};
