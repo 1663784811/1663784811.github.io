aa123a969090b8cac5688d4abe5994c4fc8d3f02
1663784811 <1663784811@qq.com>
2024-10-06 23:23:41 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@d7f11f8898c300ed4c32d9a9efd7e8ed018f8630 🚀

---
+++ assets/01.软件初始化设置.html-6CsbXZh3.js
@@ -0,0 +1,31 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as s,a as i}from"./app-tvkRXK6f.js";const a={},l=i(`<h2 id="npm" tabindex="-1"><a class="header-anchor" href="#npm" aria-hidden="true">#</a> npm</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>
+<span class="token comment">#更换镜像源</span>
+<span class="token function">npm</span> config <span class="token builtin class-name">set</span> registry https://npm.aliyun.com/
+<span class="token comment"># 腾讯:http://mirrors.cloud.tencent.com/npm/</span>
+<span class="token comment"># 华为：https://mirrors.huaweicloud.com/repository/npm/</span>
+<span class="token comment"># 中科大镜像:https://registry.npmjs.org/</span>
+<span class="token comment"># 淘宝镜像：https://registry.npmmirror.com</span>
+
+
+<span class="token comment">#禁用 SSL 严格模式</span>
+<span class="token function">npm</span> config <span class="token builtin class-name">set</span> strict-ssl <span class="token boolean">false</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+插件
+===================================== 开发软件 
+ID 插件
+Lombok
+代码补全
+GenerateAllSetter
+代码预览
+CodeGlance
+接口
+RestfulToolkit
+日志打印
+Grep Console
+Maven 包冲突
+Dependency Analyzer
+类关系图
+Diagrams
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),d=[l];function c(r,t){return e(),s("div",null,d)}const o=n(a,[["render",c],["__file","01.软件初始化设置.html.vue"]]);export{o as default};
