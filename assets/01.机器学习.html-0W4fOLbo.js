2ee5fe2e5d69c27afb3718c001d2fcb58d652147
1663784811 <1663784811@qq.com>
2024-12-16 23:44:14 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@eef994a3a48acdd2424e440a3943bf7f7831757b 🚀

---
+++ assets/01.机器学习.html-0W4fOLbo.js
@@ -0,0 +1,4 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as r,c as t,a}from"./app-XTH9fhBy.js";const c="/assets/img01-rzP_8GvQ.png",o={},n=a(`<h4 id="范围" tabindex="-1"><a class="header-anchor" href="#范围" aria-hidden="true">#</a> 范围</h4><pre><code>人工智能 &gt; 机器学习 &gt; 深度学习
+</code></pre><p><img src="`+c+`" alt="img01.png"></p><h4 id="算法" tabindex="-1"><a class="header-anchor" href="#算法" aria-hidden="true">#</a> 算法</h4><pre><code>1.神经网络(深度学习)
+     卷积神经网络
+</code></pre>`,5),_=[n];function s(d,i){return r(),t("div",null,_)}const l=e(o,[["render",s],["__file","01.机器学习.html.vue"]]);export{l as default};
