94f3048e84de48df71ecc325bd3bb00a4effda39
1663784811 <1663784811@qq.com>
2025-01-08 04:36:43 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@41722d7f3ca6ab098c4f0ed2495d5a1998e3c54d 🚀

---
+++ assets/14.STM32SDIO.html-6ko8wxtK.js
@@ -0,0 +1,12 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as s,o as d,c as r,b as e,d as n,e as t,a as l}from"./app-K-eMSB5C.js";const c={},o=e("h3",{id:"资料",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#资料","aria-hidden":"true"},"#"),n(" 资料")],-1),h={href:"https://doc.embedfire.com/products/link/zh/latest/mcu/stm32/ebf_stm32f103_zhinanzhe/download/stm32f103_zhinanzhe.html",target:"_blank",rel:"noopener noreferrer"},m=l(`<h3 id="sd卡分类" tabindex="-1"><a class="header-anchor" href="#sd卡分类" aria-hidden="true">#</a> SD卡分类</h3><p>接口： spi sdio</p><h3 id="sdio-接口" tabindex="-1"><a class="header-anchor" href="#sdio-接口" aria-hidden="true">#</a> SDIO 接口</h3><p>8根线</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1.    D02   ----   数据2
+2.    D03   ----   数据3
+3.    CMD   ----   命令
+4.    VCC   ----   电源VDD
+5.    CLK   ----   时钟线
+6.    GND   ----   接地VSS
+7.    D00   ----   数据0
+8.    D01   ----   数据1
+9.    ---   ----   ---
+    
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5);function v(u,_){const a=s("ExternalLinkIcon");return d(),r("div",null,[o,e("p",null,[e("a",h,[n("指南者资料"),t(a)])]),m])}const f=i(c,[["render",v],["__file","14.STM32SDIO.html.vue"]]);export{f as default};
--- assets/28.C__STL.html-vimNszOG.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as t}from"./app-QSPzE-Ok.js";const c={};function o(r,n){return e(),t("div")}const f=_(c,[["render",o],["__file","28.C__STL.html.vue"]]);export{f as default};
