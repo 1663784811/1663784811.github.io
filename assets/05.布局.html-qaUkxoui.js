96c7aba0642f549325768680bfa3dfec15bb8628
1663784811 <1663784811@qq.com>
2025-01-22 21:39:30 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@4879b6a1e75a433b98b17822d141740a9b557382 🚀

---
+++ assets/05.布局.html-qaUkxoui.js
@@ -0,0 +1,20 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as i,a as n}from"./app-fpcHT6LC.js";const d={},r=n(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+1.线性布局（LinearLayout）：将子视图按照水平或垂直方向进行排列。
+
+2.相对布局（RelativeLayout）：允许子视图相对于父视图或其他子视图进行定位。
+
+3.帧布局（FrameLayout）：在单个视图上堆叠子视图，每个子视图覆盖前一个子视图。
+
+4.表格布局（TableLayout）：将子视图放置在行和列的网格中。
+
+5.网格布局（GridLayout）：将子视图放置在规则的网格中，可以指定每个单元格的行数和列数。
+
+6.约束布局（ConstraintLayout）：使用约束条件定义子视图之间的关系，可以在不同屏幕尺寸上提供灵活的布局。
+
+7.帧布局（CoordinatorLayout）：是一个扩展的相对布局，用于处理交互式效果和用户界面行为的容器布局。
+
+8.绝对布局（AbsoluteLayout）：使用固定的坐标位置来放置子视图，已不推荐使用。
+
+=======================================   常用组件
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_1-线性布局" tabindex="-1"><a class="header-anchor" href="#_1-线性布局" aria-hidden="true">#</a> 1.线性布局</h3><h3 id="_2-相对布局" tabindex="-1"><a class="header-anchor" href="#_2-相对布局" aria-hidden="true">#</a> 2.相对布局</h3><h3 id="_3-帧布局" tabindex="-1"><a class="header-anchor" href="#_3-帧布局" aria-hidden="true">#</a> 3.帧布局</h3><h3 id="_6-约束布局" tabindex="-1"><a class="header-anchor" href="#_6-约束布局" aria-hidden="true">#</a> 6.约束布局</h3><h3 id="coordinatorlayout" tabindex="-1"><a class="header-anchor" href="#coordinatorlayout" aria-hidden="true">#</a> CoordinatorLayout</h3>`,6),s=[r];function l(t,c){return a(),i("div",null,s)}const v=e(d,[["render",l],["__file","05.布局.html.vue"]]);export{v as default};
