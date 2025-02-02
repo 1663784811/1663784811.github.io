2a5ad2275d96416c10568c6f87a7f6bd1b97b652
1663784811 <1663784811@qq.com>
2025-02-02 21:02:05 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@cd14e4419da91a765698b7d8544ace1d714bd68e 🚀

---
+++ assets/计算机网络.html-J4nkzTaa.js
@@ -0,0 +1,21 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as a,a as n}from"./app-SNqnN-Pf.js";const r={},d=n(`<h2 id="网络模型osi" tabindex="-1"><a class="header-anchor" href="#网络模型osi" aria-hidden="true">#</a> 网络模型OSI</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>物理层（Physical Layer）：负责传输数据的物理媒介和比特流，例如电缆、光纤、无线信号等。
+
+数据链路层（Data Link Layer）：提供对物理层传输的比特流进行分帧和差错校验，确保可靠的数据传输，例如以太网协议。
+    以太网（Ethernet）：是最常用的局域网技术，定义了数据链路层的帧格式和MAC地址的使用规则。
+    PPP（Point-to-Point Protocol）：用于在点对点连接上建立和维护数据链路连接，例如拨号连接或串行链路。
+    HDLC（High-Level Data Link Control）：一种数据链路层协议，用于在点对点或点对多点的通信链路上提供可靠的数据传输。
+    SLIP（Serial Line Internet Protocol）：一种简单的数据链路层协议，通常用于串行线路上的IP数据传输。
+    PPPoE（Point-to-Point Protocol over Ethernet）：在以太网上运行的一种协议，将PPP封装在以太网帧中，用于在宽带接入网络中建立和维护连接。
+    Wi-Fi（Wireless Fidelity）：无线局域网技术，定义了无线数据链路层的帧格式和协议，如IEEE 802.11系列标准。
+网络层（Network Layer）：处理分组的路由和转发，实现不同网络之间的数据传输，例如IP协议。
+
+传输层（Transport Layer）：提供端到端的可靠数据传输，包括分段、流量控制和错误恢复，例如TCP协议和UDP协议。
+
+会话层（Session Layer）：建立、管理和终止会话（连接）中的通信会话，例如会话控制协议。
+
+表示层（Presentation Layer）：处理数据的表示格式，包括数据加密、压缩和格式转换，例如加密协议和压缩协议。
+
+应用层（Application Layer）：提供网络应用服务和用户接口，例如HTTP协议、FTP协议、SMTP协议等。
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="常用的网络协议" tabindex="-1"><a class="header-anchor" href="#常用的网络协议" aria-hidden="true">#</a> 常用的网络协议</h2><h3 id="arp" tabindex="-1"><a class="header-anchor" href="#arp" aria-hidden="true">#</a> ARP</h3><h3 id="ip" tabindex="-1"><a class="header-anchor" href="#ip" aria-hidden="true">#</a> IP</h3><h3 id="icmp" tabindex="-1"><a class="header-anchor" href="#icmp" aria-hidden="true">#</a> ICMP</h3><h3 id="tcp-udp" tabindex="-1"><a class="header-anchor" href="#tcp-udp" aria-hidden="true">#</a> TCP UDP</h3><h2 id="网络字节序" tabindex="-1"><a class="header-anchor" href="#网络字节序" aria-hidden="true">#</a> 网络字节序</h2><h3 id="大端序列-和-小端序列" tabindex="-1"><a class="header-anchor" href="#大端序列-和-小端序列" aria-hidden="true">#</a> 大端序列 和 小端序列</h3><h2 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a></h2>`,10),s=[d];function l(t,c){return i(),a("div",null,s)}const v=e(r,[["render",l],["__file","计算机网络.html.vue"]]);export{v as default};
