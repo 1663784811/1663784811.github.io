03e51ef8bd87fc4823215c9fcd8dbd2f283760b6
1663784811 <1663784811@qq.com>
2025-02-03 07:30:23 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@2ec584c33b7b50fa2ab164b99fc6114c296c6c64 🚀

--- assets/01.项目架构.html-nu5JhJSf.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as d,a as i}from"./app-V7ZioKQY.js";const n={},r=i(`<h2 id="页面设计" tabindex="-1"><a class="header-anchor" href="#页面设计" aria-hidden="true">#</a> 页面设计</h2><p>屏保页面</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>室内温度、 室内湿度
-
-天气
-
-
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="锁屏页面-九宫格解锁" tabindex="-1"><a class="header-anchor" href="#锁屏页面-九宫格解锁" aria-hidden="true">#</a> 锁屏页面（ 九宫格解锁 ）</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
-
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="门铃猫眼页面" tabindex="-1"><a class="header-anchor" href="#门铃猫眼页面" aria-hidden="true">#</a> 门铃猫眼页面</h2><h2 id="设置页面" tabindex="-1"><a class="header-anchor" href="#设置页面" aria-hidden="true">#</a> 设置页面</h2><h2 id="主页面-设备" tabindex="-1"><a class="header-anchor" href="#主页面-设备" aria-hidden="true">#</a> 主页面 （ 设备 ）</h2>`,8),s=[r];function t(c,l){return a(),d("div",null,s)}const u=e(n,[["render",t],["__file","01.项目架构.html.vue"]]);export{u as default};
