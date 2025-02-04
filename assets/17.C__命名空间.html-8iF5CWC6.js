4bff9c74f71a86c1da16499cc668808b1eebc6a2
1663784811 <1663784811@qq.com>
2025-02-04 01:33:28 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@cec3234ca34900771fd6b205fe7aeffd799969c9 🚀

---
+++ assets/17.C__命名空间.html-8iF5CWC6.js
@@ -0,0 +1,16 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as n,a}from"./app-LWWWdjBV.js";const d={},s=a(`<h3 id="命名空间定义" tabindex="-1"><a class="header-anchor" href="#命名空间定义" aria-hidden="true">#</a> 命名空间定义</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>namespace 空间名{
+    int a = 1;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="命名空间使用" tabindex="-1"><a class="header-anchor" href="#命名空间使用" aria-hidden="true">#</a> 命名空间使用</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>方式1：
+空间名::a;
+
+方式2
+using namespace 空间名;
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),l=[s];function r(c,t){return i(),n("div",null,l)}const m=e(d,[["render",r],["__file","17.C__命名空间.html.vue"]]);export{m as default};
---
+++ assets/31.C__STL_deque双端数组.html-SqPAosmP.js
@@ -0,0 +1,1 @@
+import{_}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as t}from"./app-LWWWdjBV.js";const c={};function o(r,n){return e(),t("div")}const f=_(c,[["render",o],["__file","31.C__STL_deque双端数组.html.vue"]]);export{f as default};
