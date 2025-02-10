fcfe669d9c02b7c78ec568faeb3d5dcee92e3975
1663784811 <1663784811@qq.com>
2025-02-10 13:02:19 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@c103cdfb7fb0595a6133424708bc0b41c8e5a6e6 🚀

---
+++ assets/01.环境搭建.html-8nSDvfMj.js
@@ -0,0 +1,12 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as i,a as s}from"./app-kZKJw7q_.js";const n="/assets/01img-kACFJvA2.png",d="/assets/02img-RqZxeeyJ.png",t="/assets/03img-O4QuVjiZ.png",r={},l=s(`<h2 id="下载jdk" tabindex="-1"><a class="header-anchor" href="#下载jdk" aria-hidden="true">#</a> 下载JDK</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>下载地址
+https://www.oracle.com/java/technologies/javase/javase8u211-later-archive-downloads.html
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="安装jdk" tabindex="-1"><a class="header-anchor" href="#安装jdk" aria-hidden="true">#</a> 安装JDK</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>安装位置
+D:\\java\\jdk1.8.0_191
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="`+n+`" alt="img.png"></p><h2 id="配置path-classpath" tabindex="-1"><a class="header-anchor" href="#配置path-classpath" aria-hidden="true">#</a> 配置path &amp; classpath</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>JAVA_HOME     D:\\java\\jdk1.8.0_191
+Path          %JAVA_HOME%\\bin
+CLASSPATH     .;%JAVA_HOME%\\lib\\dt.jar;%JAVA_HOME%\\lib\\toos.jar
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="`+d+`" alt="02img.png"></p><h2 id="测试" tabindex="-1"><a class="header-anchor" href="#测试" aria-hidden="true">#</a> 测试</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>java
+javac
+java -version
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="`+t+'" alt="03img.png"></p>',11),c=[l];function v(o,m){return a(),i("div",null,c)}const p=e(r,[["render",v],["__file","01.环境搭建.html.vue"]]);export{p as default};
---
+++ assets/03.流程设计器.html-5hE13wAi.js
@@ -0,0 +1,1 @@
+import{_}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as t}from"./app-kZKJw7q_.js";const c={};function o(r,n){return e(),t("div")}const f=_(c,[["render",o],["__file","03.流程设计器.html.vue"]]);export{f as default};
