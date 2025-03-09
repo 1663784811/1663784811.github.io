20e64c4d16008e3f8f055dcf4a16367ba89e6ed3
1663784811 <1663784811@qq.com>
2025-03-09 17:00:54 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@f8cd4fdf7b9a69ea7f577614f1d04087df0ef0c7 🚀

---
+++ assets/02.电阻_电容_电感.html-DJwnSxGo.js
@@ -0,0 +1,14 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as i,a as d}from"./app-im815PFY.js";const n={},s=d(`<h3 id="电阻" tabindex="-1"><a class="header-anchor" href="#电阻" aria-hidden="true">#</a> 电阻</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>一个限流元件,它可限制通过它所连支路的电流大小
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h5 id="参数" tabindex="-1"><a class="header-anchor" href="#参数" aria-hidden="true">#</a> 参数</h5><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1.电阻大小 （欧姆）
+2.功率     最大可承受分得的电压
+3.
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="电容" tabindex="-1"><a class="header-anchor" href="#电容" aria-hidden="true">#</a> 电容</h3><h5 id="参数-1" tabindex="-1"><a class="header-anchor" href="#参数-1" aria-hidden="true">#</a> 参数</h5><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1.容量大小     （法拉）
+2.类型
+3.容抗 
+    
+    
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="电感" tabindex="-1"><a class="header-anchor" href="#电感" aria-hidden="true">#</a> 电感</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1.感抗
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,9),r=[s];function l(t,c){return a(),i("div",null,r)}const h=e(n,[["render",l],["__file","02.电阻_电容_电感.html.vue"]]);export{h as default};
