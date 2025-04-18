eb55047ca343c971913c2ced763d3d94e065b426
1663784811 <1663784811@qq.com>
2025-04-18 13:04:52 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@1f4be00bd567f428051b781b87df0cb3e5e76750 🚀

---
+++ assets/08.Mqtt认证.html-9zCwOsav.js
@@ -0,0 +1,18 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as n,o as s,c as i,b as e,d as r,e as d,a as c}from"./app-ZNz1llaj.js";const l={},h={href:"https://github.com/rabbitmq/rabbitmq-auth-backend-http",target:"_blank",rel:"noopener noreferrer"},u=c(`<h3 id="第一步-开启插件" tabindex="-1"><a class="header-anchor" href="#第一步-开启插件" aria-hidden="true">#</a> 第一步：开启插件</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>rabbitmq-plugins <span class="token builtin class-name">enable</span> rabbitmq_auth_backend_http
+rabbitmq-plugins <span class="token builtin class-name">enable</span> rabbitmq_auth_backend_cache
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="第二步-修改配置" tabindex="-1"><a class="header-anchor" href="#第二步-修改配置" aria-hidden="true">#</a> 第二步：修改配置</h3><p>/etc/rabbitmq/rabbitmq.conf</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>auth_backends.1 = http
+auth_http.http_method   = post
+auth_http.user_path     = http://some-server/auth/user
+auth_http.vhost_path    = http://some-server/auth/vhost
+auth_http.resource_path = http://some-server/auth/resource
+auth_http.topic_path    = http://some-server/auth/topic
+
+
+auth_backends.1 = cache
+auth_cache.cached_backend = http
+auth_cache.cache_ttl = 5000
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="第三步-重启服务" tabindex="-1"><a class="header-anchor" href="#第三步-重启服务" aria-hidden="true">#</a> 第三步：重启服务</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>systemctl restart rabbitmq-server
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,7);function b(o,v){const a=n("ExternalLinkIcon");return s(),i("div",null,[e("p",null,[e("a",h,[r("参考文档"),d(a)])]),u])}const _=t(l,[["render",b],["__file","08.Mqtt认证.html.vue"]]);export{_ as default};
---
+++ assets/index.html-2Hzph7la.js
@@ -0,0 +1,43 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as e,a as d}from"./app-ZNz1llaj.js";const a={},s=d(`<h2 id="鱼头笔记" tabindex="-1"><a class="header-anchor" href="#鱼头笔记" aria-hidden="true">#</a> 鱼头笔记</h2><p>&quot;鱼头笔记&quot; 是一个个人技术博客，主要分享与编程、硬件和项目开发相关的内容。</p><p>包含常用的互联网技术</p><p>java后台</p><p>vue 前端</p><p>硬件单片机 、linux</p><p>涵盖了微控制器资源、 通信协议（如 USART、I2C、SPI 等）、 硬件组件（EEPROM、LCD、传感器）以及软件工具和项目工作流程的详细信息。</p><h2 id="软件" tabindex="-1"><a class="header-anchor" href="#软件" aria-hidden="true">#</a> 软件</h2><p>项目架构 ================== 服务监控 ===================</p><h4 id="skywalking" tabindex="-1"><a class="header-anchor" href="#skywalking" aria-hidden="true">#</a> SkyWalking</h4><h4 id="elaticsearch-、logstash-、kibana" tabindex="-1"><a class="header-anchor" href="#elaticsearch-、logstash-、kibana" aria-hidden="true">#</a> ElaticSearch 、Logstash 、Kibana</h4><h4 id="node-export" tabindex="-1"><a class="header-anchor" href="#node-export" aria-hidden="true">#</a> node export</h4><h2 id="硬件" tabindex="-1"><a class="header-anchor" href="#硬件" aria-hidden="true">#</a> 硬件</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>单片机资源:
+
+
+通信协议:
+    usart
+    I2C
+    CAN
+    SPI
+    ADC
+    DAC
+    DMA ( stm32 )
+    FSMC ( 灵活静态存储器控制器 )
+    
+具体硬件:
+   EEPROM
+   FLASH
+   文件系统
+       FAT
+   LCD
+   触摸屏 (  电容屏、电阻屏  )
+   SD卡 ( TF卡 )  
+   MPU6050 (  陀罗仪  )
+   摄像头
+   电机
+       无刷电机
+       步进电机
+       伺服电机
+   音频播放
+       读取mp3文件-&gt; 解码为数字信号 -&gt; DAC 转为模拟信号 -&gt; 功率放大 -&gt; 扬声器
+   WIFI
+   蓝牙
+   Zigbee
+   GSM
+   LoRa
+   NFC
+
+实物
+    打印机
+    无人机
+    云台
+        
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,14),l=[s];function r(v,c){return n(),e("div",null,l)}const m=i(a,[["render",r],["__file","index.html.vue"]]);export{m as default};
