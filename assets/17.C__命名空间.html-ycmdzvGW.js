a3406256e455b353ed2766ce8e158efe7e3c4212
1663784811 <1663784811@qq.com>
2025-12-02 06:39:41 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@f8b908c0dc53870242bfdcb19582b9f25d40797e 🚀

---
+++ assets/17.C__命名空间.html-ycmdzvGW.js
@@ -0,0 +1,21 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,a as e}from"./app-MZh4GzO8.js";const i={},c=e(`<h3 id="命名空间定义" tabindex="-1"><a class="header-anchor" href="#命名空间定义" aria-hidden="true">#</a> 命名空间定义</h3><div class="language-cpp line-numbers-mode" data-ext="cpp"><pre class="language-cpp"><code><span class="token keyword">namespace</span> 空间名<span class="token punctuation">{</span>
+    <span class="token keyword">int</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+    
+    <span class="token keyword">class</span> <span class="token class-name">MyClass</span> <span class="token punctuation">{</span>
+        <span class="token keyword">public</span><span class="token operator">:</span>
+            <span class="token keyword">void</span> <span class="token function">sayHello</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="命名空间使用" tabindex="-1"><a class="header-anchor" href="#命名空间使用" aria-hidden="true">#</a> 命名空间使用</h3><div class="language-cpp line-numbers-mode" data-ext="cpp"><pre class="language-cpp"><code>方式<span class="token number">1</span>：
+空间名<span class="token double-colon punctuation">::</span>a<span class="token punctuation">;</span>
+
+方式<span class="token number">2</span>
+<span class="token keyword">using</span> <span class="token keyword">namespace</span> 空间名<span class="token punctuation">;</span>
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h3><div class="language-cpp line-numbers-mode" data-ext="cpp"><pre class="language-cpp"><code>
+
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),l=[c];function d(p,t){return s(),a("div",null,l)}const u=n(i,[["render",d],["__file","17.C__命名空间.html.vue"]]);export{u as default};
---
+++ assets/17.C__枚举.html-jtDQBP41.js
@@ -0,0 +1,10 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a}from"./app-MZh4GzO8.js";const d={},s=a(`<h2 id="枚举-定义" tabindex="-1"><a class="header-anchor" href="#枚举-定义" aria-hidden="true">#</a> 枚举 定义</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>enum 枚举名{
+    傎1，
+    傎2，
+    傎3
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="使用" tabindex="-1"><a class="header-anchor" href="#使用" aria-hidden="true">#</a> 使用</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>enum 枚举名 变量名 = 枚举值;
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),r=[s];function l(t,c){return n(),i("div",null,r)}const o=e(d,[["render",l],["__file","17.C__枚举.html.vue"]]);export{o as default};
