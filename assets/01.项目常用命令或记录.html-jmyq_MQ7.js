389f7339dd82e293cb07d06f85b3874c5af7a330
1663784811 <1663784811@qq.com>
2025-12-11 14:21:24 +0000

    Deploying to gh-pages from @ 1663784811/1663784811.github.io@f0627e6f1f023284034bc708c3a62b8c5e91f9f0 🚀

---
+++ assets/01.项目常用命令或记录.html-jmyq_MQ7.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,a as s}from"./app-sOVZqPqZ.js";const a={},l=s(`<h2 id="maven" tabindex="-1"><a class="header-anchor" href="#maven" aria-hidden="true">#</a> maven</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+## 打包安装到本地
+mvn clean install -DskipTests
+
+mvn clean install deploy -DskipTests 
+
+## 修改版本号
+mvn versions:set -DnewVersion=&quot;1.0.1&quot;
+
+
+
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),d=[l];function r(v,c){return n(),i("div",null,d)}const o=e(a,[["render",r],["__file","01.项目常用命令或记录.html.vue"]]);export{o as default};
